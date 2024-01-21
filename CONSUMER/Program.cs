deec6f0ecfd9e39fc7c6c886a11f94e164de0257
MatheusTN <matheus.naka@modalgr.com.br>
2024-01-21 01:36:13 -0300

    first commit

---
+++ CONSUMER/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace CONSUMER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definimos uma conexão com um nó RabbitMQ em localhost
+            var factory = new ConnectionFactory()
+            {
+                  HostName = "localhost",
+                  UserName = "guest",
+                  Password = "guest"
+            };
+
+            // Abrimos uma conexão com um nó RabbitMQ
+            using var connection = factory.CreateConnection();
+
+            // Criar o canal na conexão para operar 
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "mensagem1", // Declaramos a fila a partir da qual vamos consumir as mensagens
+                durable: false,     
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            // Solicita a entrega das mensagens de forma assíncrona e fornece um retorno de chamada
+            var consumer = new EventingBasicConsumer(channel);
+
+            // Recebe a mensagem da fila
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine($"Mensagem recebida com sucesso !!");
+                Console.WriteLine(message);
+            };
+
+            // Indicamos o consumo da mensagem
+            channel.BasicConsume(
+                queue: "mensagem1",
+                autoAck: true,
+                consumer: consumer
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PUBLISHER/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace PUBLISHER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definimos uma conexão com um nó RabbitMQ em localhost
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            // Abrimos uma conexão com um nó RabbitMQ
+            using var connection = factory.CreateConnection();
+
+            // Criamos um canal onde vamos definir uma fila, uma mensagem e publicar a mensagem 
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "mensagem1", // Nome da fila
+                durable: false,     // Quando true, a fila permanece ativa após o servidor ser reiniciado
+                exclusive: false,   // Quando true, a fila só pode ser acessada via conexão atual e são excluída ao fechar a conexão
+                autoDelete: false,  // Quando true, a fila será automaticamente deletada após os consumidores usarem a fila 
+                arguments: null
+            );
+
+            string message = "Exemplo de mensagem RabbitMQ !!";
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "mensagem1",
+                basicProperties: null,
+                body: body
+            );
+
+            Console.WriteLine("Mensagem enviada com sucesso !!");
+        }
+    }
+}
